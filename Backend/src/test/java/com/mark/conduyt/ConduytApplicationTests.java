7619f147f05c41dbc59445719ecbcb603fd36def
conduytfsp <conduytfsp@gmail.com>
2026-07-09 21:24:49 +0530

    Initialized with Template files

---
+++ Backend/src/test/java/com/mark/conduyt/ConduytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.conduyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduytApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
