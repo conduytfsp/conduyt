f4900f6ce909901fb21f5b9dbefa03b7fc7eca86
Madhushree Banerjee <madhushreebanerjee213@gmail.com>
2026-08-10 20:17:16 +0530

    Add freelancers page and update app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,50 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LandingLayout from "./layouts/LandingLayout";
-import Home from "./pages/Home";
+import FindWork from "./pages/FindWork";
+import Freelancers from "./pages/Freelancers";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import FreelancerDashboard from "./pages/FreelancerDashboard";
-import ClientDashboard from "./pages/ClientDashboard";
 import Help from "./pages/Help";
-import FindWork from "./pages/FindWork";
-import Dashboard from "./pages/Dashboard";
-import ProfileView from "./pages/ProfileView";
-import ClientProfile from "./pages/ClientProfile";
+import ClientDashboard from "./pages/ClientDashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-
         {/* Home Page */}
         <Route path="/" element={<LandingLayout />} />
 
         {/* Find Work Page */}
         <Route path="/find-work" element={<FindWork />} />
 
-        {/* Other Pages */}
-        <Route path="/login" element={<Login />} />
+        {/* Freelancers Page */}
+        <Route path="/freelancers" element={<Freelancers />} />
 
+        {/* Authentication */}
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
+        {/* Help */}
         <Route path="/help" element={<Help />} />
 
+        {/* Client Dashboard */}
         <Route
-          path="/client-dashboard"
+          path="/client-dashboard/*"
           element={<ClientDashboard />}
         />
-        {/* Freelancer Page */}
-         <Route path="/freelancerdashboard/*" element={<FreelancerDashboard />} />
-          <Route path="/dashboard/*" element={<Dashboard />} />
-
-        {/* {ProfileView} */}
-        <Route path="/profileView/*" element={<ProfileView />} />
-        {/* {Client Profile} */}
-        <Route
-  path="/client-profile/:clientId"
-  element={<ClientProfile />}
-/>
       </Routes>
     </Router>
   );
